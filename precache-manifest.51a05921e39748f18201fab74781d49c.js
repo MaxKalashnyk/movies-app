09f19c24a90c9e2748387cd5f13368ec82fe8237
kalash14 <maxim.kalashnyk@gmail.com>
2019-07-24 00:50:53 +0300

    Updates

---
+++ precache-manifest.51a05921e39748f18201fab74781d49c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e42bd56d88bb4b4553d42a52ba12927a",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "2ed162d331b96f4336a7",
+    "url": "/movies-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1720a2dfae46d1d106fc",
+    "url": "/movies-app/static/css/main.a0a667cd.chunk.css"
+  },
+  {
+    "revision": "2ed162d331b96f4336a7",
+    "url": "/movies-app/static/js/2.23f650f7.chunk.js"
+  },
+  {
+    "revision": "1720a2dfae46d1d106fc",
+    "url": "/movies-app/static/js/main.5de7f223.chunk.js"
+  },
+  {
+    "revision": "f58da00be1d28636e05a",
+    "url": "/movies-app/static/js/runtime~main.41022d0a.js"
+  }
+]);
